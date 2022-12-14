a44e5cd87d63f1678455d5bd9122ead98b6b0bd0
Jihwan Kim <fankim@icloud.com>
2022-12-14 19:56:43 +0900

    Update : 좌표계 실제 화면에 맞게 재설정

--- sketch.js
+++ sketch.js
@@ -4,33 +4,39 @@ let p = {
   r: 20,
   vx: 0.8,
   vy: 1.3,
+  color: "red",
 };
-const CANVAS_WIDTH = 300;
-const CANVAS_HEIGHT = 300;
+const CANVAS_WIDTH = 360;
+const CANVAS_HEIGHT = 415;
+const SCREEN_WIDTH = 151;
+const SCREEN_HEIGHT = 158;
+const SCREEN_OFFSET = { x: 104, y: 150 };
+const SCREEN_COORDINATE = {};
+const SCREEN_COLOR = "gray";
+let bgImage;
+function preload() {
+  bgImage = loadImage("background.png");
+}
 function setup() {
   createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+  image(bgImage, 0, 0);
   noFill();
   noStroke();
   smooth();
 }
 function draw() {
-  background(200, 141, 30);
-  if (dist(p.x, p.y, mouseX, mouseY) < p.r) {
-    fill(255, 200, 130);
-  } else {
-    fill(10, 20, 130);
-  }
-
+  fill(SCREEN_COLOR);
+  translate(SCREEN_OFFSET.x, SCREEN_OFFSET.y);
+  rect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+  fill(p.color);
   ellipse(p.x, p.y, p.r * 2, p.r * 2);
-  // fill(0, 0, 0);
-  // ellipse(p.x, p.y, 10, 10);
-  if (p.x + p.r > CANVAS_WIDTH) {
+  if (p.x + p.r > SCREEN_WIDTH) {
     p.vx *= -1;
   }
   if (p.x - p.r < 0) {
     p.vx *= -1;
   }
-  if (p.y + p.r > CANVAS_HEIGHT) {
+  if (p.y + p.r > SCREEN_HEIGHT) {
     p.vy *= -1;
   }
   if (p.y - p.r < 0) {
